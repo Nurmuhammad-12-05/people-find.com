23937e0231dc856a5dbfdeb097b73b1f35daeae9
Nurmuhammad <ashuraliyevnurmuhammad16@gmail.com>
2025-07-17 16:29:19 +0500

    update .env and github.strategies.ts

--- src/modules/auth/strategies/github.strategies.ts
+++ src/modules/auth/strategies/github.strategies.ts
@@ -6,9 +6,15 @@ import { Profile, Strategy } from 'passport-github2';
 export class GithubStrategi extends PassportStrategy(Strategy, 'github') {
   constructor() {
     super({
-      clientID: process.env.CLIENT_ID_GITHUB as string,
-      clientSecret: process.env.CLIENT_SECRET_GITHUB as string,
-      callbackURL: process.env.CLIENT_COLLBACK_URL_GITHUB as string,
+      clientID:
+        (process.env.CLIENT_ID_GITHUB_DEV as string) ||
+        (process.env.CLIENT_ID_GITHUB_PROD as string),
+      clientSecret:
+        (process.env.CLIENT_SECRET_GITHUB_DEV as string) ||
+        (process.env.CLIENT_SECRET_GITHUB_PROD as string),
+      callbackURL:
+        (process.env.CLIENT_COLLBACK_URL_GITHUB_DEV as string) ||
+        (process.env.CLIENT_COLLBACK_URL_GITHUB_PROD as string),
       scope: ['user:email', 'profile'],
     });
   }
