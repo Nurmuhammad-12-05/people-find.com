2662ebfed994e1931f1171ee714e2369c98750fd
Nurmuhammad <ashuraliyevnurmuhammad16@gmail.com>
2025-07-18 05:27:30 +0500

    update linkdin.strategies.ts

--- src/modules/auth/auth.module.ts
+++ src/modules/auth/auth.module.ts
@@ -3,10 +3,10 @@ import { AuthService } from './auth.service';
 import { AuthController } from './auth.controller';
 import { GoogleStrategi } from './strategies/google.strategies';
 import { GithubStrategi } from './strategies/github.strategies';
-import { LinkedinStrategy } from './strategies/linkedin.strategies';
+// import { LinkedinStrategy } from './strategies/linkedin.strategies';
 
 @Module({
   controllers: [AuthController],
-  providers: [AuthService, GoogleStrategi, GithubStrategi, LinkedinStrategy],
+  providers: [AuthService, GoogleStrategi, GithubStrategi],
 })
 export class AuthModule {}
